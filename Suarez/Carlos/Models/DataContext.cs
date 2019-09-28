ef23d3d181a523fb3db739dee8819529f13f3476
Bruno <brunoivanzaratefernandez@gmail.com>
2019-09-28 12:14:22 -0400

    Commit final

---
+++ Suarez/Carlos/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace Carlos.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConecction")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Carlos.Models.Clown> Clowns { get; set; }
+    }
+}
