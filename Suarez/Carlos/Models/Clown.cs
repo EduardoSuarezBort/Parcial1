aa1311c374e1204b244e41e5ba520beea9d378a9
Bruno <brunoivanzaratefernandez@gmail.com>
2019-09-28 12:04:59 -0400

    Commit Model

---
+++ Suarez/Carlos/Models/Clown.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Carlos.Models
+{
+    public enum TypeTricks
+    {
+        Casa,
+        Cama,
+        Camisa
+    }
+    public class Clown
+    {
+        [Key]
+        public int ClownID { get; set; }
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        [StringLength(24,MinimumLength =2)]
+        public string NickName { get; set; }
+        [Required]
+        public TypeTricks Tricks { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Birthdate { get; set; }
+
+    }
+}
